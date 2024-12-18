f79a5dc75b4fb884c276f7924b638dbb0b820f7c
Be-soraj <sorajbomrel@gmail.com>
2024-12-18 15:10:38 +0545

    what happy

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="w-full flex items-center justify-center">
+      <Text className="text-lg p-10 w-5/6 text-center">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit
+        doloremque dolor amet reprehenderit accusantium enim omnis, soluta
+        repellendus nesciunt, sit non culpa ut ea ex tenetur temporibus hic
+        possimus voluptatem?
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
